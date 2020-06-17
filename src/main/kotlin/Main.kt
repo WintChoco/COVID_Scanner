eb290f8af8975c4a60de4c4256dc5dd698f83de0
WintChoco <64053781+WintChoco@users.noreply.github.com>
2020-06-17 21:29:26 +0900

    Update reload

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,22 +3,32 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 import java.awt.BorderLayout
 import java.awt.Button
+import java.awt.Container
 import java.awt.Dimension
 import java.awt.event.ActionListener
 import java.io.IOException
-import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JLabel
 
 
 fun main() {
     val frame = JFrame("COVID Scanner")
-    load(frame)
+    val c = frame.contentPane
+    val COVID = JLabel("Reloading")
+    label(c, COVID)
+    val button = Button("[ RELOAD ]")
+    button.addActionListener(ActionListener { _ ->
+        label(c, COVID)
+    })
+    c.add(button, BorderLayout.SOUTH)
+    frame.setLocation(500, 500)
+    frame.preferredSize = Dimension(740, 100)
+    frame.pack()
+    frame.isVisible = true
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 }
 
-fun load(frame: JFrame) {
-    println("로드되었습니다!")
-    val c = frame.contentPane
+fun label(c: Container, COVID: JLabel) {
     val url = "https://www.worldometers.info/coronavirus/"
     var doc: Document? = null
     try {
@@ -28,20 +38,8 @@ fun load(frame: JFrame) {
     }
     val element: Elements? = doc?.select("div.maincounter-number")
     val span: String = element!!.select("span").html().replace("\n", "|")
-    println(span)
     val s = span.split("|").toTypedArray()
-    val COVID = JLabel("Coronavirus Cases : " + s[0] + " | Deaths : " + s[1] + " | Recovered : " + s[2])
+    COVID.text = "Coronavirus Cases : " + s[0] + " | Deaths : " + s[1] + " | Recovered : " + s[2]
     COVID.font = COVID.font.deriveFont(20.0f)
     c.add(COVID)
-    val button = Button("[ RELOAD ]")
-    button.addActionListener(ActionListener { e ->
-        JFrame.EXIT_ON_CLOSE
-        load(frame)
-    })
-    c.add(button, BorderLayout.SOUTH)
-    frame.setLocation(0, 0)
-    frame.preferredSize = Dimension(740, 100)
-    frame.pack()
-    frame.isVisible = true
-    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 }
